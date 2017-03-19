3f7de3d5e331dc92dcfabd4910d6ddbbe5ea6298
Guy Lyons <guylyons2@gmail.com>
2017-03-18 23:01:39 -0400

    add in templates

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
+var app = express();
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
