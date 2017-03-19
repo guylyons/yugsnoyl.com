7d0dafe3f296f65a3091cfdc9baf8fea0a7fe276
Guy Lyons <guylyons2@gmail.com>
2017-03-18 22:59:59 -0400

    add handlebars

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000);
