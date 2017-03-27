c067c2d7858427be92d66a533f33cfd114babd14
Guy Lyons <guylyons2@gmail.com>
2017-03-26 23:14:32 -0400

    deps

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <section>
+        <h2>About ME</h2>
+    </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass">
+    body
+        border: 2px solid pink
+</style>
