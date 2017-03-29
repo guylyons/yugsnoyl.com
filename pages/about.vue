8fa54f5847caf48be6270f4bda789995edf31973
Guy Lyons <guylyons2@gmail.com>
2017-03-29 08:38:18 -0400

    add readme

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template lang="html">
     <section>
-        <h2>About ME</h2>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
 </template>
 
@@ -10,6 +11,4 @@ export default {
 </script>
 
 <style lang="sass">
-    body
-        border: 2px solid pink
 </style>
---
+++ readme.md
@@ -0,0 +1,17 @@
+# yugsnoyl.com
+
+This repo hosts the contents of my personal site, which is currently built with Nuxt.js
+
+Built with [Vue.js][1] & [Nuxt.js][2]
+
+## Getting Started
+
+	npm install
+	npm start
+
+## TODO
+- Add WebPack
+- Finish About
+
+[1]:	https://vuejs.org
+[2]:	https://nuxtjs.org
